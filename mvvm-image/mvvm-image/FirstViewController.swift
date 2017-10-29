3acf755bc56d68977869eac4f6af56e1527c1702
Daniel No <mr.danielno@gmail.com>
2017-10-29 15:57:12 -0400

    add cocoapods

---
+++ mvvm-image/mvvm-image/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  mvvm-image
+//
+//  Created by Daniel No on 10/29/17.
+//  Copyright © 2017 Daniel No. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
